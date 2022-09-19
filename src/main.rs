08e4084adedcca35022541e9f02a291021b9e8b3
R <ryansereno@protonmail.com>
2022-09-19 09:47:01 -0400

    refactored counter function

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use hmacsha1::hmac_sha1;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::{thread, time};
 use std::io;
-use base32::decode;
+use base32::{encode, decode};
 
 const TOTP_DURATION: u128 = 30000; 
 const TOTP_LENGTH: u32 = 6;
@@ -17,8 +17,6 @@ fn main() ->io::Result<()> {
     // TOTP private keys are usually shared in the Base32 encoded form. This is to prevent human
     // error when reading and inputing. Base32 is alphanumeric ONLY and is in all caps.
     // This base32 string needs to be decoded prior to passing into the HMAC algorithm
-    let encoded_key = decode(base32::Alphabet::Crockford, &key);
-     //println!("encoded key {}", encoded_key);
 
     loop {
         let totp = generate_htop(key.clone(), get_count());
@@ -27,7 +25,18 @@ fn main() ->io::Result<()> {
     }
 }
 
-fn create_message_from_counter(mut counter: u128) -> [u8; 8] {
+fn current_unix_time() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Current time not available")
+        .as_millis()
+}
+
+fn get_count() -> u128 {
+    current_unix_time() / TOTP_DURATION
+}
+
+fn counter_to_bits(mut counter: u128) -> [u8; 8] {
     let mut buff = [0; 8];
     for i in 0..8 {
         buff[7 - i] = (counter & 0xff) as u8;
@@ -46,21 +55,12 @@ fn dynamic_truncation(bytes: &[u8]) -> u32 {
 }
 
 fn generate_htop(key: String, counter: u128) -> u32 {
-    let message = create_message_from_counter(counter);
+    let message = counter_to_bits(counter);
     let hmac_value = hmac_sha1(key.as_bytes(), &message);
     let dynamic_truncation = dynamic_truncation(&hmac_value);
 
     dynamic_truncation % 10u32.pow(TOTP_LENGTH)
 }
 
-fn current_timestamp() -> u128 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Current time not available")
-        .as_millis()
-}
 
-fn get_count() -> u128 {
-    current_timestamp() / TOTP_DURATION
-}
 
